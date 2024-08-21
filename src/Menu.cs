3ed3a324d12595b6655427f4e4d154dede5efb7c
martodele703 <mdeleon@agtium.com>
2024-08-21 11:59:55 -0300

    Class Menu

--- src/Menu.cs
+++ src/Menu.cs
@@ -8,5 +8,46 @@ using System.Collections;
 /// </summary>
 public class Menu
 {
-    private ArrayList dishes = new ArrayList();
+    private List<Dish> dishes = new List<Dish>();
+
+    public List<Dish> Dishes
+    {
+        get { return dishes;}
+        set
+        {
+            if (value != null)
+            {
+                dishes = value;
+            }
+        }
+        
+    }
+    public void AddDish(Dish dish)
+    {
+        this.dishes.Add(dish);
+    }
+
+    public void RemoveDish(Dish dish)
+    {
+        this.dishes.Remove(dish);
+    }
+
+    public Dish GetDishByName(string name)
+    {
+        bool flag = true;
+        int i = 0;
+        
+        while (flag || i < this.dishes.Count)
+        {
+            if (this.dishes[i].Name == name)
+            {
+                flag = false;
+            }
+
+            i++;
+        }
+
+        if (flag) return null;
+        else return this.dishes[i];
+    }
 }
