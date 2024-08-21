28b295642a7aca46379133bf181eb0539cc05e9c
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2024-08-21 12:52:54 -0300

    Waiter branch

--- src/Waiter.cs
+++ src/Waiter.cs
@@ -7,5 +7,27 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Waiter
 {
+    public string Name { get; set; }
     private ArrayList assignedTables = new ArrayList();
+    
+    public Waiter(string name)
+    {
+        Name = name;
+        assignedTables = new ArrayList();
+    }
+
+    public void AssignTable(Table table)
+    {
+        assignedTables.Add(table);
+        Console.WriteLine($"La mesa {table} fue agregada al mozo {Name}");
+    }
+
+    public void TakeOrder(Table table, Dish dish)
+    {
+        if (assignedTables.Contains(table))
+        {
+            table.AddToOrder(dish);
+        }
+    }
+    
 }
