bf83f45123690fcb1f0289147d86738b3a920b98
rami-lopez <ramiro.lopez@correo.ucu.edu.uy>
2024-08-21 12:56:49 -0300

    Waiter solucionado

--- src/Dish.cs
+++ src/Dish.cs
@@ -5,5 +5,14 @@ namespace Ucu.Poo.Restaurant;
 /// </summary>
 public class Dish
 {
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public bool IsVegetarian { get; set; }
     
+    public Dish(string dish, decimal price, bool isVegetarian)
+    {
+        Name = dish;
+        Price = price;
+        IsVegetarian = isVegetarian;
+    }
 }
--- src/Table.cs
+++ src/Table.cs
@@ -7,9 +7,29 @@ using System.Collections;
 /// </summary>
 public class Table
 {
+    public int TableNumber { get; set; }
+    public bool IsOccupied { get; private set; }
+    
     private ArrayList order = new ArrayList();
+    
+    public Table(int tableNumber)
+    { 
+        this.TableNumber = tableNumber;
+        this.IsOccupied = false;
+    }
+
+    public void Ocupy()
+    {
+        this.IsOccupied = true;
+    }
+    
     public bool HasOrders()
     {
         return this.order.Count > 0;
+    }
+
+    public void AddToOrder(Dish dish)
+    {
+        order.Add(dish);
     }
 }
